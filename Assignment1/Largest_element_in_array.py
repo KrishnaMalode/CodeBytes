8c939fda995537bd725cabd9f817c10fce562c63
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-10-20 01:16:28 +0530

    Update Largest_element_in_array.py
    Handled invalid input

--- Assignment1/Largest_element_in_array.py
+++ Assignment1/Largest_element_in_array.py
@@ -1,24 +1,25 @@
 
 ## method 1 = use sorted function to sort elements in list in ascending order and return last element
-def largest_element(list):
-    list = sorted(list)
-    return print(f'{list[n-1]} is the largest element in the array')
+def largest_element(list1):
+    n = len(list1)
+    s_list = sorted(list1)
+    return print(f'{s_list[n-1]} is the largest element in the array')
 
-largest_element(list)
+
 
 ## method 2 = store first element in list in variable (max) and swap if greater element is found in list while iterting over it
-def largest_element2(list):
-    max = list[0]
-
+def largest_element2(list1):
+    max = list1[0]
+    n = len(list1)
     for i in range(0,n):
-        if max<list[i]:
-            max = list[i]
+        if max<list1[i]:
+            max = list1[i]
     print(f'{max} is the largest element in the array')
 
-largest_element2(list)
+
 
 def main():
-    list = []
+    ip_list = []
     n = int(input("Enter no of elements you want to add in list: "))
 
     for i in range(0,n):
@@ -26,9 +27,11 @@ def main():
             ele = int(input())
             if (type(ele)!= int):
                 raise ValueError("Enter an integer")
-            list.append(ele)
+            ip_list.append(ele)
         except ValueError as e:
             print(f'Invalid input {e}')
+    largest_element(ip_list)
+    largest_element2(ip_list)
 
 if __name__ == "__main__":
     main()
