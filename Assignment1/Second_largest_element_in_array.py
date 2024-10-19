1ec8c29f7873ab4eece6b1202490cc03fd59d960
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-10-20 00:56:25 +0530

    Update Second_largest_element_in_array.py
    Handled invalid input and edge cases

--- Assignment1/Second_largest_element_in_array.py
+++ Assignment1/Second_largest_element_in_array.py
@@ -1,17 +1,28 @@
-list = []
-n = int(input("Enter no of elements you want to add in list: "))
 
-for item in range(0,n):
-    ele = int(input())
-    list.append(ele)
+def main():
+    list = []
+    n = int(input("Enter no of elements you want to add in list: "))
 
-print(f'list = {list}')
+    for item in range(0,n):
+        try:
+            ele = int(input())
+            if (type(ele)!= int):
+                raise ValueError("Enter an integer")
+            list.append(ele)
+        except ValueError as e:
+            print(f'Invalid input {e}')
 
-## method 1 = use sorted function to sort elements in list in ascending order and return last element
+    print(f'list = {list}')
+    second_largest_element(list)
+
+## method 1 = use sorted function to sort elements in list in ascending order and return second last element
 def second_largest_element(list):
+    n = len(list)  # n has length of list
     list = sorted(list)
     return print(f'{list[n-2]} is the second largest element in the array')
 
-second_largest_element(list)
 
 
+
+if __name__ =="__main__":
+    main()
