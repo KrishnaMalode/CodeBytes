dd95ad962df39ce35722fdf9c290b5e20da68e79
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-10-26 17:16:31 +0530

    Update 1752.Check_if_Array_Is_Sorted_and_Rotated.py
    added check if was sorted initially or not

--- Assignment1/1752.Check_if_Array_Is_Sorted_and_Rotated.py
+++ Assignment1/1752.Check_if_Array_Is_Sorted_and_Rotated.py
@@ -1,19 +1,26 @@
 ## to check if the array is sorted
 def solution(list):
-    #step1 = sort array in ascending order
-    sorted_list = sorted(list)
-    sol = False
-    # to rotate in possible combinations from 0 to len(list)
-    for k in range(0,len(list)+1):
-        # for left rotation of list i.e. taking last k elements and rotating them to start of the list, using list slicing 
-        rotated_list = sorted_list[k:] + sorted_list[:k]
-        if(rotated_list == list):
-            sol = True
-    
-    if (sol == True):
-        print(f'list {list} was sorted and rotated')
+    #step1 = check if condition sorted initial i>i-1 index
+    count = 0
+    for i in range(0,len(list)+1):
+        if list[i+1]<list[i]:
+            count += 1
+        sorted_list = sorted(list)
+    if count ==1:
+        sol = False
+        # to rotate in possible combinations from 0 to len(list)
+        for k in range(0,len(list)+1):
+            # for left rotation of list i.e. taking last k elements and rotating them to start of the list, using list slicing 
+            rotated_list = sorted_list[k:] + sorted_list[:k]
+            if(rotated_list == list):
+                sol = True
+        
+        if (sol == True):
+            print(f'list {list} was sorted and rotated')
+        else:
+            print(f'list {list} was NOT sorted and rotated')
     else:
-        print(f'list {list} was NOT sorted and rotated')
+        print(f'{list} was not sorted initially before rotation')
 
 
 def main():
